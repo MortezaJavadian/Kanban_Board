71e8e73620f7fef376ba073105050bd060f8f7dc
MortezaJavadian <mor83teza@gmail.com>
2024-05-26 13:15:59 +0330

    Create project and make view and cerate butten + Add item with details

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Kanban_Board
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
