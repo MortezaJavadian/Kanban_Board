3cd0b13c2d63b518ea9e0fa7d068ab258f6775b5
MortezaJavadian <mor83teza@gmail.com>
2024-05-27 09:44:01 +0330

    Add button ... in top of every column

--- obj/Debug/net8.0-windows/Kanban-Board.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Kanban-Board.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kanban-Board")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e8e73620f7fef376ba073105050bd060f8f7dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kanban-Board")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kanban-Board")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
